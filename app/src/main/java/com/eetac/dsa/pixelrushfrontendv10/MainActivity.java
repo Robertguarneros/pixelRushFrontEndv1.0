97d08492724713b96281925837d255aecee70e12
LuciaOcana <lucia.ocana@estudiantat.upc.edu>
2023-11-21 19:19:15 +0100

    version 1.1.0 - register añadido

--- app/src/main/java/com/eetac/dsa/pixelrushfrontendv10/MainActivity.java
+++ app/src/main/java/com/eetac/dsa/pixelrushfrontendv10/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.eetac.dsa.pixelrushfrontendv10.backEndClasses.LoginCredentials;
+import com.eetac.dsa.pixelrushfrontendv10.backEndClasses.RegisterCredentials;
 
 import java.util.List;
 
@@ -37,6 +38,12 @@ public class MainActivity extends AppCompatActivity {
         editTextUsername = findViewById(R.id.editTextUsername);
 
         editTextPassword = findViewById(R.id.editTextPassword);
+
+        editTextName = findViewById(R.id.editTextName);
+
+        editTextMail = findViewById(R.id.editTextmail);
+        editTextAge = findViewById(R.id.editTextAge);
+        editTextSurname = findViewById(R.id.editTextsurname);
 
 
         buttonRegister= findViewById(R.id.registerBtn);
@@ -68,6 +75,39 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<Void> call, Throwable t) {
                 System.out.println("Error: "+t.getMessage());
+                Toast.makeText(MainActivity.this,"Error"+t.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void register(View view){
+        PixelRushService pixelRushService = PixelRushService.retrofit.create(PixelRushService.class);//creating interface
+        String name = editTextName.getText().toString();
+        String surname = editTextSurname.getText().toString();
+        String mail = editTextMail.getText().toString();
+        String username = editTextUsername.getText().toString();
+        String password = editTextPassword.getText().toString();
+        String age = editTextAge.getText().toString();
+        int ageInt = Integer.parseInt(age);
+
+        RegisterCredentials singinCredentials = new RegisterCredentials(username,password,name,surname,mail,ageInt);
+
+        Call<Void> callsingin = pixelRushService.register(singinCredentials);
+
+        callsingin.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    Log.i("iteracion1","User register successful");
+                    Toast.makeText(MainActivity.this,"User register successful",Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.i("iteracion1","Error: " + response.code() + " " + response.message());
+                    Toast.makeText(MainActivity.this,"Error"+response.message(),Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Log.i("iteracion1","Error: "+t.getMessage(),t);
                 Toast.makeText(MainActivity.this,"Error"+t.getMessage(),Toast.LENGTH_SHORT).show();
             }
         });
